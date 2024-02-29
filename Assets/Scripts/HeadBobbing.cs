56c68c6205a8953300a2942b8f2bce48a16f0f0e
HollowCain <gogmag@mail.ru>
2024-02-29 16:57:43 +0300

    Initial commit

---
+++ Assets/Scripts/HeadBobbing.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadBobbing : MonoBehaviour
+{
+
+    public float bobbingSpeed = 0.18f;
+    public float bobbingHeight = 0.2f;
+    public float midpoint = 1.8f;
+
+    private float timer = 0.0f;
+
+    void Update()
+    {
+        if (!transform.parent.gameObject.GetComponent<CharacterController>().isGrounded) return;
+        float waveslice = 0.0f;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 cSharpConversion = transform.localPosition;
+
+        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
+        {
+            timer = 0.0f;
+        }
+        else
+        {
+            waveslice = Mathf.Sin(timer);
+            timer = timer + bobbingSpeed;
+            if (timer > Mathf.PI * 2)
+            {
+                timer = timer - (Mathf.PI * 2);
+            }
+        }
+        if (waveslice != 0)
+        {
+            float translateChange = waveslice * bobbingHeight;
+            float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+            translateChange = totalAxes * translateChange;
+            cSharpConversion.y = midpoint + translateChange;
+        }
+        else
+        {
+            cSharpConversion.y = midpoint;
+        }
+
+        transform.localPosition = cSharpConversion;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class Player_Movement : MonoBehaviour
+{
+    public float playerWalkingSpeed = 5f;
+    public float playerRunningSpeed = 15f;
+    public float jumpStrenght = 5f;
+    public float verticalRotationLimit = 55f;
+
+    float forwardMovement;
+    float sidewaysMovement;
+
+    float verticalVelocity;
+
+    float verticalRotation = 0;
+    CharacterController cc;
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontalRotation, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y");
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        if (cc.isGrounded) {
+            forwardMovement = Input.GetAxis("Vertical") * playerWalkingSpeed;
+            sidewaysMovement = Input.GetAxis("Horizontal") * playerWalkingSpeed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                forwardMovement = Input.GetAxis("Vertical") * playerRunningSpeed;
+                sidewaysMovement = Input.GetAxis("Horizontal") * playerRunningSpeed;
+            }
+        }
+        
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        if (Input.GetButton("Jump") && cc.isGrounded)
+        {
+            verticalVelocity = jumpStrenght;
+        }
+
+        Vector3 playerMovement = new Vector3(sidewaysMovement, verticalVelocity, forwardMovement);
+
+        
+
+        cc.Move(transform.rotation * playerMovement * Time.deltaTime);
+
+    }
+}
